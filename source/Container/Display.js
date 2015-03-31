205bac9602085fe6077aa1b3a3b6c8bed512f71d
Jerome Vial <jerome.vial@gmail.com>
2015-03-31 15:02:49 +0200

    Add event minimised, normalized and maximised

--- source/Container/Display.js
+++ source/Container/Display.js
@@ -4,6 +4,10 @@
  */
 UI.Container.implement({
 
+	/**
+	 * Display options for container
+	 * @type {Object} options
+	 */
 	options: {
 		display: {
 			fx: {
@@ -70,7 +74,7 @@ UI.Container.implement({
 	 * [toggle description]
 	 * @return {[type]} [description]
 	 */
-	toggle: function() {
+	toggleDisplay: function() {
 
 		if (this._display == 'normalized')
 			this.minimize();
@@ -86,8 +90,11 @@ UI.Container.implement({
 	minimize: function() {
 		//_log('minimize');
 
+		
 		if (!this.display)
 			this._initDisplay();
+
+		this.fireEvent('minimized');
 
 		this.display.fx.start(0);
 
@@ -98,7 +105,7 @@ UI.Container.implement({
 
 		minimal.settings.save();
 
-		this.fireEvent('display', 'minimized');
+		this.fireEvent('minimized');
 	},
 
 	/**
@@ -107,6 +114,7 @@ UI.Container.implement({
 	 */
 	normalize: function(size) {
 		//_log('normalize');
+		this.fireEvent('normalize');
 
 		var size = size || this.options.width || 280;
 		if (this._display == 'normalized') return;
@@ -123,7 +131,7 @@ UI.Container.implement({
 		minimal.settings.set('layout.' + this.main + '.display', 'normalized');
 		minimal.settings.save();
 
-		this.fireEvent('display', 'normalized');
+		this.fireEvent('normalized');
 
 	},
 
