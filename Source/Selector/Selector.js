71bc3094b356eb18dfba8eefe34079b500d6d59e
Bruno Santos <bsantos@floor.ch>
2015-12-09 12:11:23 +0100

    documentation + fix syntax errors

--- Source/Selector/Selector.js
+++ Source/Selector/Selector.js
@@ -1,9 +1,6 @@
-
 /**
  * UI Selector Class
  * @class UI.Selector
- * @extends {UI.Component}
- * @type {Class}
  */
 define([
 	'UI/Selector/Border',
@@ -42,7 +39,7 @@ define([
 			menu: false,
 			border: {
 				size: 1,
-				display:'none'
+				display: 'none'
 			},
 			resizer: true,
 			overlay: false,
@@ -51,7 +48,7 @@ define([
 				path: '/'
 			},
 			usefx: false,
-			enable : true,
+			enable: true,
 			timerOnHide: 0,
 			onCatch: {},
 			onDisable: {},
@@ -59,7 +56,12 @@ define([
 			positionning: 'relative'
 		},
 
-		initialize: function(options){
+		/**
+		 * initialize
+		 * @param  {Object} options
+		 * @return {void}
+		 */
+		initialize: function(options) {
 			//_log.debug('UI.Selector.initilize()');
 			this.setOptions(options);
 
@@ -70,7 +72,7 @@ define([
 			var scope = this.options.scope || this.container;
 			var target = this.options.target;
 
-			this.name = this.options.prefix+'-'+this.options.name;
+			this.name = this.options.prefix + '-' + this.options.name;
 			this.size = {};
 
 			this.timer = null;
@@ -78,32 +80,32 @@ define([
 			//_log.debug('UI.Selector.init(scope,target)',this.name);
 
 			this._initElement(this.options.components);
-			this._initEvents(scope,target);
+			this._initEvents(scope, target);
 
 			//_log.debug('shoud hide this');
 
 			this.hideNow();
 		},
 
-		update: function(){
+		update: function() {
 
 			var scope = this.options.scope;
 			var target = this.options.target;
 
-			this._initEvents(scope,target);
+			this._initEvents(scope, target);
 		},
 
-		_initEvents: function(scope,target) {
+		_initEvents: function(scope, target) {
 			//_log.debug('UI.Selector._initEvents(scope,target)',typeOf(scope),target);
-			var delay = 20;
+			//var delay = 20;
 			var self = this;
 
 			//_log.debug('UI.Selector._initEvents(scope,target)',typeOf(scope),target);
 
-			var delegation = self.options.trigger+':relay('+target+')';
+			var delegation = self.options.trigger + ':relay(' + target + ')';
 
 			scope.addEvent(delegation, function(ev, el) {
-				self.reach(el)
+				self.reach(el);
 			});
 
 			/*var list = scope.querySelectorAll( target );
@@ -139,13 +141,13 @@ define([
 
 		attachElement: function(el) {
 			//_log.debug('UI.Selector._initEvents(scope,target)',scope,target);
-			var delay = 20;
+			//var delay = 20;
 			var self = this;
 
 
 			//_log.debug('UI.Selector._setEventsElement',el,self.options.trigger);
 
-			el.addEvent(self.options.trigger, function(){
+			el.addEvent(self.options.trigger, function() {
 				self.reach(el);
 			});
 
@@ -167,27 +169,31 @@ define([
 		},
 
 
-		_initElement: function(components){
+		_initElement: function(components) {
 			var self = this;
 
 			this.wrapper = new Element('div', {
-				'class': this.options.wrapper.clss,
-				zIndex: this.options.zIndex
-			}).set('data-selector', this.options.name)
-			.inject(this.options.container, 'top');
+					'class': this.options.wrapper.clss,
+					zIndex: this.options.zIndex
+				}).set('data-selector', this.options.name)
+				.inject(this.options.container, 'top');
 
-			components.each(function(name){
+			components.each(function(name) {
 				//_log.debug('Selector _initElement',name);
 				self.options[name].content = self.options.container;
-				var build = 'build'+name.capitalize();
-				if (!self.options[name].usefx)
+				var build = 'build' + name.capitalize();
+				if (!self.options[name].usefx) {
 					self.options[name].usefx = self.options.usefx;
+				}
 
 				self.selectors.push(self[build](self.options[name]));
 			});
 
-			if (this.isEnable()) this.enable();
-			else this.disable();
+			if (this.isEnable()) {
+				this.enable();
+			} else {
+				this.disable();
+			}
 
 			return this.wrapper;
 		},
@@ -197,7 +203,7 @@ define([
 
 		},
 
-		buildBorder: function(options){
+		buildBorder: function(options) {
 			var self = this;
 			//_log.debug();
 
@@ -206,15 +212,25 @@ define([
 			this.border = new Border(this.wrapper, options);
 
 			this.addEvents({
-				show: function(){ self.border.show(); },
-				hide: function(){ self.border.hide(); },
-				reach: function(el){ self.border.reach(el); },
-				repos: function(el){ self.border.reach(el); },
-				highlight: function(color){	self.border.highlight(color); }
+				show: function() {
+					self.border.show();
+				},
+				hide: function() {
+					self.border.hide();
+				},
+				reach: function(el) {
+					self.border.reach(el);
+				},
+				repos: function(el) {
+					self.border.reach(el);
+				},
+				highlight: function(color) {
+					self.border.highlight(color);
+				}
 			});
 		},
 
-		buildMask: function(opts){
+		buildMask: function(opts) {
 			var self = this;
 
 			opts.positionning = this.options.positionning;
@@ -226,18 +242,28 @@ define([
 
 			this.mask.addEvent('click', function(ev) {
 				self.fireEvent('click', ev);
-			}); 
+			});
 
 			this.addEvents({
-				show: function(){ self.mask.show(); },
-				hide: function(){ self.mask.hide(); },
-				reach: function(el){	self.mask.reach(el); },
-				repos: function(el){	self.mask.reach(el); },
-				highlight: function(color){	self.mask.highlight(color); }
+				show: function() {
+					self.mask.show();
+				},
+				hide: function() {
+					self.mask.hide();
+				},
+				reach: function(el) {
+					self.mask.reach(el);
+				},
+				repos: function(el) {
+					self.mask.reach(el);
+				},
+				highlight: function(color) {
+					self.mask.highlight(color);
+				}
 			});
 		},
 
-		buildResizer: function(options){
+		buildResizer: function(options) {
 			var self = this;
 
 			this.resizer = new Resizer(this.wrapper, options).addEvents({
@@ -250,25 +276,33 @@ define([
 			});
 
 			this.addEvents({
-				show: function(el){ self.resizer.show(); },
-				hide: function(el){	self.resizer.hide(); },
-				reach: function(el){ self.resizer.reach(el); },
-				repos: function(el){ self.resizer.reach(el); }
+				show: function(el) {
+					self.resizer.show();
+				},
+				hide: function(el) {
+					self.resizer.hide();
+				},
+				reach: function(el) {
+					self.resizer.reach(el);
+				},
+				repos: function(el) {
+					self.resizer.reach(el);
+				}
 			});
 		},
 
-		buildMenu: function(options){
+		buildMenu: function(options) {
 			//_log.debug('buildMenu', this.options.name, options);
 
-			var self = this,
-				left = null,
-				right = null;
+			var self = this;
+			//var left = null;
+			//var right = null;
 
 			// _log.debug( this.wrapper, options);
 
 			options.positionning = this.options.positionning;
 
-			this.menu = new Menu( this.wrapper, options);
+			this.menu = new Menu(this.wrapper, options);
 
 			if (this.options.timerOnHide) {
 				this.menu.element.addEvents({
@@ -282,25 +316,35 @@ define([
 			}
 
 			this.addEvents({
-				show: function(){ self.menu.element.show(); },
-				hide: function(){	self.menu.element.hide(); },
-				reach: function(el) { self.menu.reach(el);	},
-				repos: function(el) { self.menu.reach(el); },
-				highlight: function(color){	self.menu.element.highlight(color); }
+				show: function() {
+					self.menu.element.show();
+				},
+				hide: function() {
+					self.menu.element.hide();
+				},
+				reach: function(el) {
+					self.menu.reach(el);
+				},
+				repos: function(el) {
+					self.menu.reach(el);
+				},
+				highlight: function(color) {
+					self.menu.element.highlight(color);
+				}
 			});
 		},
 
 
-		buildStatus: function(options){
+		buildStatus: function(options) {
 			//_log.debug('buildMenu', this.options.name, options);
 
-			var self = this,
-				left = null,
-				right = null;
+			var self = this;
+			//var left = null;
+			//var right = null;
 
 			//_log.debug( this.wrapper);
 
-			this.status = new Status( this.wrapper, options);
+			this.status = new Status(this.wrapper, options);
 
 			if (this.options.timerOnHide) {
 				this.status.element.addEvents({
@@ -314,15 +358,25 @@ define([
 			}
 
 			this.addEvents({
-				show: function(){ self.status.element.show(); },
-				hide: function(){	self.status.element.hide(); },
-				reach: function(el) { self.status.reach(el); },
-				repos: function(el) { self.status.reach(el); },
-				highlight: function(color){	self.status.element.highlight(color); }
+				show: function() {
+					self.status.element.show();
+				},
+				hide: function() {
+					self.status.element.hide();
+				},
+				reach: function(el) {
+					self.status.reach(el);
+				},
+				repos: function(el) {
+					self.status.reach(el);
+				},
+				highlight: function(color) {
+					self.status.element.highlight(color);
+				}
 			});
 		},
 
-		_initOverlay: function(){
+		_initOverlay: function() {
 			var self = this;
 
 			this.overlay = new Overlay({
@@ -332,7 +386,7 @@ define([
 					self.fireEvent('click', self.el);
 				},
 				dblclick: function() {
-					self.fireEvent('dblclick',self.el);
+					self.fireEvent('dblclick', self.el);
 				}
 			});
 
@@ -351,51 +405,68 @@ define([
 			}
 
 			this.addEvents({
-				show: function(el){ self.overlay.show(); },
-				hide: function(el){	self.overlay.hide(); },
-				reach: function(el){ self.overlay.reach(el); },
-				repos: function(el){ self.overlay.reach(el); },
-				highlight: function(color){	self.overlay.highlight(color); }
+				show: function(el) {
+					self.overlay.show();
+				},
+				hide: function(el) {
+					self.overlay.hide();
+				},
+				reach: function(el) {
+					self.overlay.reach(el);
+				},
+				repos: function(el) {
+					self.overlay.reach(el);
+				},
+				highlight: function(color) {
+					self.overlay.highlight(color);
+				}
 			});
 		},
 
-		reach: function(el){
+		reach: function(el) {
 			if (el) {
 				this.el = el;
 			} else if (this.el) {
 				el = this.el;
-			} else { return; }
+			} else {
+				return;
+			}
 
 			//_log.debug('reach',el);
 
 			if (this.isEnable) {
 				this.show();
-				this.fireEvent('reach',el);
+				this.fireEvent('reach', el);
 			}
 		},
 
-		repos: function(el){
+		repos: function(el) {
 			if (el) {
 				this.el = el;
 			} else if (this.el) {
 				el = this.el;
-			} else { return; }
+			} else {
+				return;
+			}
 
 			if (this.isEnable) {
 				this.show();
-				this.fireEvent('repos',el);
+				this.fireEvent('repos', el);
 			}
 		},
 
-		set: function(name,value,name){
-			//_log.debug(name,value,name);
+		set: function(name, value, name) {
+			//_log.debug(name, value, name);
 
-			if (name)
-				self[name][name](value);
-			else
-			this.selectors.each(function(name) {
-				self[name][name](value);
-			});
+			var self = this;
+
+			if (name) {
+				this[name][name](value);
+			} else {
+				this.selectors.each(function(name) {
+					self[name][name](value);
+				});
+			}
 
 			return this;
 		},
@@ -413,27 +484,29 @@ define([
 
 		},
 
-		hide: function(){
+		hide: function() {
 			clearTimeout(this.timer);
 			this.timer = (function() {
 				this.fireEvent('hide');
-			}).delay(this.options.timerOnHide,this);
+			}).delay(this.options.timerOnHide, this);
 		},
 
-		hideNow: function(){
+		hideNow: function() {
 			clearTimeout(this.timer);
 			this.fireEvent('hide');
 		},
 
-		show: function(){
+		show: function() {
 			clearTimeout(this.timer);
-			if (this.isEnable)
+			if (this.isEnable) {
 				this.fireEvent('show');
+			}
 		},
 
-		highlight: function(color){
-			if (this.isEnable)
-				this.fireEvent('highlight',color);
+		highlight: function(color) {
+			if (this.isEnable) {
+				this.fireEvent('highlight', color);
+			}
 		},
 
 		enable: function(selector) {
@@ -453,16 +526,23 @@ define([
 		},
 
 		isEnable: function() {
-			if (Cookie.read(this.name) === '1') return true;
-				else return false;
+			if (Cookie.read(this.name) === '1') {
+				return true;
+			} else {
+				return false;
+			}
 		},
 
-		toggle: function(){
-			if (Cookie.read(this.name) === '1') this.disable();
-			else this.enable();
+		toggle: function() {
+			if (Cookie.read(this.name) === '1') {
+				this.disable();
+			} else {
+				this.enable();
+			}
 		}
+
 	});
 
 	return exports;
-});
 
+});
