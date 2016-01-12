731e92f3f6bfd49f119a84e8e924c0390527e57a
Jerome Vial <jerome.vial@gmail.com>
2016-01-12 22:20:51 +0100

    remove logs

--- Source/Menu/Menu.js
+++ Source/Menu/Menu.js
@@ -13,7 +13,8 @@ define([
 	ButtonControl
 ) {
 
-	var _log = __debug('ui-menu').defineLevel();
+	var _log = __debug('ui-menu');
+	//	_log.defineLevel();
 
 	var exports = new Class({
 
@@ -51,7 +52,7 @@ define([
 		 * @return {Object}
 		 */
 		initialize: function(options) {
-			_log.debug('menu init', options);
+			//_log.debug('menu init', options);
 
 			this.setOptions(options);
 
