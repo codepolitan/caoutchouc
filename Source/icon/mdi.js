a3c35fbcde0e9d40c0105c95ec2e662ee7b8b88a
Jerome Vial <jerome.vial@gmail.com>
2016-01-12 22:21:44 +0100

    add icon ref

---
+++ Source/icon/mdi.js
@@ -0,0 +1,22 @@
+(function(root, factory) {
+	if (typeof define === 'function' && define.amd) {
+		define([], factory);
+	} else if (typeof exports === 'object') {
+		module.exports = factory();
+	}
+}(this, function() {
+
+	return {
+		power: 'mdi-action-power',
+		side: 'mdi-action-chrome-reader-mode',
+		checked: 'mdi-check-box',
+		sprints: 'mdi-action-run',
+		package: 'mdi-package',
+		planning: 'mdi-calendar-clock',
+		notes: 'mdi-app-notes',
+		files: 'mdi-app-files',
+		news: 'mdi-app-news',
+		template: 'mdi-app-template'
+	};
+
+}));
