48d26e58a9bfc7f446f996d8555a56a37becbedb
Bruno Santos <bsantos@floor.ch>
2016-05-27 14:29:30 +0200

    split toolbar code into methods

--- Source/Toolbar/Toolbar.js
+++ Source/Toolbar/Toolbar.js
@@ -5,7 +5,7 @@
  */
 define(function(require, exports, module) {
 
-  var appIconConfig = require('config/icon/app');
+  //var appIconConfig = require('config/icon/app');
   var fontIconConfig = require('config/icon/font');
   var mdiIconConfig = require('config/icon/mdi');
   var langControlsConfigEn = require('config/sys/lang/control/en');
@@ -16,8 +16,14 @@ define(function(require, exports, module) {
 
   var Toolbar = new Class({
 
+    options: {
+      toolbar: {
+        list: []
+      }
+    },
+
     /**
-     * Initialize Toolbar
+     * initialize toolbar
      * @param {Object} obj
      */
     _initToolbar: function(obj) {
@@ -33,11 +39,6 @@ define(function(require, exports, module) {
 
       this._initToolbarReady(obj);
 
-      if (!obj.list) {
-        _log.warn('missing list field');
-        return;
-      }
-
       for (var i = 0; obj.list.length > i; i++) {
         var name = obj.list[i];
         var bar = obj[name];
@@ -46,32 +47,44 @@ define(function(require, exports, module) {
           continue;
         }
 
-        //_log.debug(name, bar.container);
+        //continue if container is not defined
+        if (!bar.container) {
+          continue;
+        }
 
+        //init container if doesn't exist
         if (!this.container[bar.container]) {
-          if (!bar.container) {
-            continue;
-          }
-
           this.container['_init' + bar.container.capitalize()]();
         }
 
-        var element = this.toolbar[name] = new Element('div', {
-          'class': 'ui-toolbar'
-        }).addEvents({
-
-        }).inject(this.container[bar.container]);
-
-        if (bar.klss) {
-          element.addClass(bar.klss);
-        }
-
-        element.addClass('toolbar-' + name);
+        var element = this._initToolbarEl(name, bar);
 
         this._initToolbarComp(bar, element);
       }
 
       this.container.fireEvent('resize');
+    },
+
+    /**
+     * init toolbar element
+     * @param  {string} name
+     * @param  {Object} bar
+     * @return {DOMElement}
+     */
+    _initToolbarEl: function(name, bar) {
+      var element = this.toolbar[name] = new Element('div', {
+        'class': 'ui-toolbar'
+      }).addEvents({
+
+      }).inject(this.container[bar.container]);
+
+      if (bar.klss) {
+        element.addClass(bar.klss);
+      }
+
+      element.addClass('toolbar-' + name);
+
+      return element;
     },
 
     /**
@@ -99,69 +112,9 @@ define(function(require, exports, module) {
     },
 
     /**
-     * [_initMore description]
-     * @return {void}
-     */
-    _initMore: function() {
-      _log.debug('_initMore', this.container, this.control.more, this.toolbar.more);
-
-      if (!this.control || !this.toolbar) {
-        _log.warn('missing control or toolbar');
-        return null;
-      }
-
-      var timer = null;
-
-      var toolbar = this.toolbar.more;
-      var control = this.control.more;
-
-      if (!control || !toolbar) {
-        _log.warn('missing control or toolbar', control, toolbar);
-        return;
-      }
-
-      control.addEvent('press', function(ev) {
-        _log.debug('-- press', ev);
-        //var coord = control.getCoordinates();
-        toolbar.setStyles({
-          display: 'inline-block'
-        });
-
-        return true;
-      });
-
-      this.addEvent('control::pressed', function() {
-        toolbar.setStyle('display', 'none');
-      });
-
-      toolbar.addEvents({
-        /**
-         * @ignore
-         */
-        mouseleave: function() {
-          clearTimeout(timer);
-          timer = setTimeout(function() {
-            toolbar.setStyle('display', 'none');
-          }, 500);
-        },
-        /**
-         * @ignore
-         */
-        mouseenter: function() {
-          clearTimeout(timer);
-        },
-        /**
-         * @ignore
-         */
-        click: function() {
-          toolbar.setStyle('display', 'none');
-        }
-      });
-    },
-
-    /**
-     * [_initToolbarComp description]
+     * init toolbar comp
      * @param {Object} bar
+     * @param {DOMElement} element
      */
     _initToolbarComp: function(bar, element) {
       _log.debug('_initToolbarComp', bar, element);
@@ -278,106 +231,176 @@ define(function(require, exports, module) {
       if (isAllow) {
         _log.debug('require module', name, clss, opts);
         this._requireModule(clss, function(Clss) {
-          //_log.debug('Clss', typeOf(Clss));
-
-          if (!Clss) {
-            _log.warn('not found, should require?', name, opts);
-            return;
-          }
-
-          self.control[name] = new Clss(opts).inject(element);
-
-          // init more toolbar menu
-          if (name === 'more') {
-            self._initMore();
-            return;
-          }
-
-          self.control[name].addEvents({
-            /**
-             * @ignore
-             */
-            change: function(value) {
-              //var name =  this.options.name;
-              //_log.debug('change', this);
-              if (this.isEnable()) {
-                self.fireEvent(name, value);
-              }
-            }
-          });
-
-          if (clss === 'UI/Control/Button') {
-            self.control[name].addEvents({
-              /**
-               * @ignore
-               */
-              press: function() {
-                _log.debug('press', name);
-                self.fireEvent('control::pressed');
-                //var name =  this.options.name;
-                //_log.debug('press', name, this.isEnable());
-                if (this.isEnable()) {
-                  self.fireEvent('control::' + name, this);
-                  self.fireEvent(name, [self]);
-                }
-              }
-            });
-          }
-
-          if (clss === 'UI/Control/Upload') {
-            self.control[name].addEvents({
-              /**
-               * @ignore
-               */
-              uploadFile: function(files) {
-                _log.debug('uploadFile', files);
-                if (this.isEnable()) {
-                  //self.fireEvent('control::'+name, this);
-                  self.fireEvent(name, [files]);
-                }
-              }
-            });
-          }
-
-          if (clss === 'UI/Control/Field') {
-            self.control[name].addEvents({
-              /**
-               * @ignore
-               */
-              change: function() {
-                //_log.debug('change', name, this.isEnable());
-                self.fireEvent('control::pressed');
-                //var name =  this.options.name;
-                //_log.debug('press', name, this.isEnable());
-                if (this.isEnable()) {
-                  self.fireEvent('control::' + name, this);
-                  self.fireEvent(name, [self]);
-                }
-              }
-            });
-          }
-
-          if (clss === 'UI/Control/ButtonMenu') {
-            self.control[name].addEvents({
-              /**
-               * @ignore
-               */
-              press: function(name) {
-                self.fireEvent('control::pressed');
-                //var name =  this.options.name;
-                _log.debug('ButtonMenu press', name, this.isEnable());
-                if (this.isEnable()) {
-                  self.fireEvent('control::' + name, this);
-                  self.fireEvent(name, [self]);
-                }
-              }
-            });
-          }
+        	self._initToolbarControl(Clss, name, clss, opts, element);
         });
 
         self.isReady++;
         self.fireEvent('isReady', self.isReady);
       }
+    },
+
+    /**
+     * _initToolbarControl
+     */
+    _initToolbarControl: function(Clss, name, clss, opts, element) {
+      //_log.debug('Clss', typeOf(Clss));
+
+      var self = this;
+
+      if (!Clss) {
+        _log.warn('not found, should require?', name, opts);
+        return;
+      }
+
+      this.control[name] = new Clss(opts).inject(element);
+
+      // init more toolbar menu
+      if (name === 'more') {
+        this._initMore();
+        return;
+      }
+
+      self.control[name].addEvents({
+        /**
+         * @ignore
+         */
+        change: function(value) {
+          //var name =  this.options.name;
+          //_log.debug('change', this);
+          if (this.isEnable()) {
+            self.fireEvent(name, value);
+          }
+        }
+      });
+
+      if (clss === 'UI/Control/Button') {
+        self.control[name].addEvents({
+          /**
+           * @ignore
+           */
+          press: function() {
+            _log.debug('press', name);
+            self.fireEvent('control::pressed');
+            //var name =  this.options.name;
+            //_log.debug('press', name, this.isEnable());
+            if (this.isEnable()) {
+              self.fireEvent('control::' + name, this);
+              self.fireEvent(name, [self]);
+            }
+          }
+        });
+      }
+
+      if (clss === 'UI/Control/Upload') {
+        self.control[name].addEvents({
+          /**
+           * @ignore
+           */
+          uploadFile: function(files) {
+            _log.debug('uploadFile', files);
+            if (this.isEnable()) {
+              //self.fireEvent('control::'+name, this);
+              self.fireEvent(name, [files]);
+            }
+          }
+        });
+      }
+
+      if (clss === 'UI/Control/Field') {
+        self.control[name].addEvents({
+          /**
+           * @ignore
+           */
+          change: function() {
+            //_log.debug('change', name, this.isEnable());
+            self.fireEvent('control::pressed');
+            //var name =  this.options.name;
+            //_log.debug('press', name, this.isEnable());
+            if (this.isEnable()) {
+              self.fireEvent('control::' + name, this);
+              self.fireEvent(name, [self]);
+            }
+          }
+        });
+      }
+
+      if (clss === 'UI/Control/ButtonMenu') {
+        self.control[name].addEvents({
+          /**
+           * @ignore
+           */
+          press: function(name) {
+            self.fireEvent('control::pressed');
+            //var name =  this.options.name;
+            _log.debug('ButtonMenu press', name, this.isEnable());
+            if (this.isEnable()) {
+              self.fireEvent('control::' + name, this);
+              self.fireEvent(name, [self]);
+            }
+          }
+        });
+      }
+    },
+
+    /**
+     * [_initMore description]
+     * @return {void}
+     */
+    _initMore: function() {
+      _log.debug('_initMore', this.toolbar.more);
+
+      if (!this.control || !this.toolbar) {
+        _log.warn('missing control or toolbar');
+        return null;
+      }
+
+      var timer = null;
+
+      var toolbar = this.toolbar.more;
+      var control = this.control.more;
+
+      if (!control || !toolbar) {
+        _log.warn('missing control or toolbar', control, toolbar);
+        return;
+      }
+
+      control.addEvent('press', function(ev) {
+        _log.debug('-- press', ev);
+        //var coord = control.getCoordinates();
+        toolbar.setStyles({
+          display: 'inline-block'
+        });
+
+        return true;
+      });
+
+      this.addEvent('control::pressed', function() {
+        toolbar.setStyle('display', 'none');
+      });
+
+      toolbar.addEvents({
+        /**
+         * @ignore
+         */
+        mouseleave: function() {
+          clearTimeout(timer);
+          timer = setTimeout(function() {
+            toolbar.setStyle('display', 'none');
+          }, 500);
+        },
+        /**
+         * @ignore
+         */
+        mouseenter: function() {
+          clearTimeout(timer);
+        },
+        /**
+         * @ignore
+         */
+        click: function() {
+          toolbar.setStyle('display', 'none');
+        }
+      });
     },
 
     /**
