2c5e064229d69e1fc2e4f135c285739eedfee2d7
Bruno Santos <bsantos@floor.ch>
2016-06-10 12:15:11 +0200

    enhance code style

--- Source/View/Layout.js
+++ Source/View/Layout.js
@@ -1,57 +1,54 @@
 /**
-* 
-* Filter View Class
-*
-* @class View.Filter
-* @author Jerome Vial, Bruno Santos
-*/
+ * Filter View Class
+ * @class View.Filter
+ * @author Jerome Vial, Bruno Santos
+ */
+define(function(require, exports, module) {
 
-define([
-	'UI/Layout/Layout'
-], function (
-	Layout
-) {
+  var Layout = require('UI/Layout/Layout');
 
-	var _log = __debug('view:core-layout');
+  var _log = __debug('view:core-layout');
 
-	var exports = new Class({
+  module.exports = new Class({
 
-		/**
-		 * [_initLayout description]
-		 * @return {[type]} [description]
-		 */
-		_initLayout: function(options) {
-			_log.debug('_initLayout');
+    /**
+     * init layout
+     * @return {void}
+     */
+    _initLayout: function(options) {
+      _log.debug('_initLayout');
 
-			var opts = options || this.options;
-			var self = this;
+      var opts = options || this.options;
+      var self = this;
 
-			this.device = opts.device;
-			//_log.debug('_initLayout', opts.layout, opts.container);
-			if (!opts.layout) return;
-			//_log.debug('--', opts.layout, opts.container);
-			var settings = opts.settings.layout;
+      this.device = opts.device;
 
-			var layout = this.layout = new Layout({
-				theme: opts.theme,
-				container: opts.container,
-				node: opts.layout,
-				settings: settings
-			}).addEvents({
-				resizer: function(name, prop, value) {
-					self.setSettings('layout.' + name + '.' + prop, value);
-				},
-				display: function(name, state) {
-					//_log.debug('display container ' + name + ' state ', state);
+      _log.debug('_initLayout', opts.layout, opts.container);
 
-					self.setSettings('layout.' + name + '.display', state);
-				}
-			});
-		
-			this.container = this.layout.container;
-			this.container.addClass('app-' + this.options.name);
-		}
-	});
+      if (!opts.layout) {
+        return;
+      }
 
-	return exports;
+      var settings = opts.settings.layout;
+
+      var layout = this.layout = new Layout({
+        theme: opts.theme,
+        container: opts.container,
+        node: opts.layout,
+        settings: settings
+      }).addEvents({
+        resizer: function(name, prop, value) {
+          self.setSettings('layout.' + name + '.' + prop, value);
+        },
+        display: function(name, state) {
+          self.setSettings('layout.' + name + '.display', state);
+        }
+      });
+
+      this.container = this.layout.container;
+      this.container.addClass('app-' + this.options.name);
+    }
+
+  });
+
 });
