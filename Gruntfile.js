075927da31eded77d164b6e6db577c183c0e407a
Bruno Santos <bsantos@floor.ch>
2016-08-05 10:02:19 +0200

    add git task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,5 +3,6 @@ module.exports = function(grunt) {
   require('./tasks/build.js')(grunt);
   require('./tasks/watch.js')(grunt);
   require('./tasks/less.js')(grunt);
+  require('./tasks/git.js')(grunt);
 
 };
