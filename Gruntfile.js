f634aa7c805a54e547ec10e08e8e25f4cd361574
Bruno Santos <brunoleugimm@gmail.com>
2016-10-19 15:22:09 +0200

    move tasks to package.json

--- Gruntfile.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(grunt) {
-
-  require('./tasks/build.js')(grunt);
-  require('./tasks/watch.js')(grunt);
-  require('./tasks/less.js')(grunt);
-  require('./tasks/git.js')(grunt);
-  require('./tasks/webpack.js')(grunt);
-
-};
